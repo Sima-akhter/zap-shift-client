8ae54f2b099433554afb155df45d1fb0db375720
Sima-Akther <saimaislambdk@gmail.com>
2025-11-20 18:12:08 +0600

    first commit

---
+++ src/Components/Logo/Logo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from '../../assets/logo.png'
+import { Link } from 'react-router'
+
+const Logo = () => {
+  return (
+    <Link>
+
+      <div className='flex items-end'>
+        <img src={logo} alt="" />
+        <h1 className='text-3xl font-bold -ms-2.5'>zapShift</h1>
+      </div>
+    </Link>
+
+  )
+}
+
+export default Logo
